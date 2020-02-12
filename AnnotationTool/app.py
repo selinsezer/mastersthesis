a82fdb880bdefcf5c3894a53962992f4d007f223
sezerse <selinsezer@gmail.com>
2020-02-12 08:32:12 +0100

    added a similarity check for annotation tool

--- AnnotationTool/app.py
+++ AnnotationTool/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for
 import pandas as pd
 import numpy as np
 import os
+from difflib import SequenceMatcher
 
 app = Flask(__name__)
 keyword = input('Please enter the keyword to work on: ')
@@ -27,6 +28,21 @@ def get_current_status():
     print("Number of neg contracts: {}".format(negative))
 
 
+def get_match_ratio(sc, to_compare):
+    s = SequenceMatcher(lambda x: x == " ", sc, to_compare)
+    if s.ratio() >= 0.6:
+        print("Found a similar({}) contract, labeling it for you...".format(s.ratio()))
+        return True
+    return False
+
+
+def check_for_function(keyword, value):
+    for index, row in annotated_df[annotated_df['annotation'] == -1].iterrows():
+        if keyword in row['solidity']:
+            annotated_df.loc[annotated_df['address'] == row['address'], 'annotation'] = value
+    annotated_df.to_csv(new_file_name, sep=',', index=False)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -34,9 +50,25 @@ def index():
             value = 1
         else:
             value = 0
+
         #todo find an efficient check here..
         source_code = annotated_df.loc[annotated_df['address'] == request.form['addr']]['solidity'].values[0]
-        annotated_df.loc[annotated_df['solidity'] == source_code, ['annotation']] = value
+        functions = annotated_df.loc[annotated_df['address'] == request.form['addr']]['function_names'].values[0]
+        annotated_df.loc[annotated_df['function_names'] == functions, ['annotation']] = value
+
+        counter = 0
+        for index, row in annotated_df[annotated_df['annotation'] == -1].iterrows():
+            counter += 1
+            if counter > 5:
+                break
+            if get_match_ratio(source_code, row['solidity']):
+                counter = 0
+                annotated_df.loc[annotated_df['address'] == row['address'], 'annotation'] = value
+                functions = annotated_df.loc[annotated_df['address'] == row['address']]['function_names'].values[0]
+                annotated_df.loc[annotated_df['function_names'] == functions, ['annotation']] = value
+                annotated_df.to_csv(new_file_name, sep=',', index=False)
+
+        annotated_df.loc[annotated_df['address'] == request.form['addr'], 'annotation'] = value
         annotated_df.to_csv(new_file_name, sep=',', index=False)
         next_row = annotated_df[annotated_df['annotation'] == -1].head(1)
         get_current_status()
@@ -52,4 +84,5 @@ def index():
 
 
 if __name__ == "__main__":
+    # check_for_function("function voteFor()  returns (bool success){", 1)
     app.run()
